b16202b5042308a0da0e01fe549f6e3b908e1be1
Your Name <mailtoaryanr@gmail.com>
2021-01-04 15:50:43 +0530

    Specialist available

--- DispatcherServices/DispatcherServices/src/main/java/com/OrderManagement/DispatcherServices/Controller/DispatcherSpecialistController.java
+++ DispatcherServices/DispatcherServices/src/main/java/com/OrderManagement/DispatcherServices/Controller/DispatcherSpecialistController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.OrderManagement.DispatcherServices.Model.Specialists;
 import com.OrderManagement.DispatcherServices.Repositories.RequestSpecialistRepository;
 import com.OrderManagement.DispatcherServices.Repositories.SpecialistProxy;
+import com.OrderManagement.DispatcherServices.Service.SpecialistService;
 
 
 @RestController
@@ -19,27 +20,19 @@ public class DispatcherSpecialistController {
 
 	
 	@Autowired
-	private SpecialistProxy specialistProxy;
+	private SpecialistService specialistService;
 	
 	@GetMapping("specialist/{specialistId}")
 	public List<Specialists> getServiceByspecialistId(@PathVariable("specialistId") int no)
 	{
-		List<Specialists> tr = specialistProxy.getServiceByspecialistId(no);
-		System.out.println("output"+tr);
+		List<Specialists> tr = specialistService.getServiceByspecialistId(no);
 		return tr;
 	}
 	
 	  @GetMapping("/specialists") 
 	  public List<Specialists> getAllAvailableSpecialists()
 	  {
-		  List<Specialists> tr = specialistProxy.getAllSpecialists();
-		  List<Specialists> availableSpecialists = new ArrayList<>();
-		  for (Specialists specialists : tr) {
-			  if(specialists.getStatus().equalsIgnoreCase("available"))
-			  {
-			availableSpecialists.add(specialists);
-			  }
-	  }
-	  return availableSpecialists; 
+		  List<Specialists> availableSpecialists = specialistService.getAllSpecialists();
+		  return availableSpecialists; 
 	  }
 }
---
+++ DispatcherServices/DispatcherServices/src/main/java/com/OrderManagement/DispatcherServices/Service/SpecialistService.java
@@ -0,0 +1,39 @@
+package com.OrderManagement.DispatcherServices.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.OrderManagement.DispatcherServices.Model.Specialists;
+import com.OrderManagement.DispatcherServices.Repositories.SpecialistProxy;
+
+@Service
+public class SpecialistService {
+	
+	@Autowired
+	SpecialistProxy specialistProxy;
+
+	public List<Specialists> getServiceByspecialistId(int no) {
+		List<Specialists> tr = specialistProxy.getServiceByspecialistId(no);
+		return tr;
+	}
+
+	public List<Specialists> getAllSpecialists() {
+		List<Specialists> tr = specialistProxy.getAllSpecialists();
+		  List<Specialists> availableSpecialists = new ArrayList<>();
+		  for (Specialists specialists : tr) {
+			  if(specialists.getStatus().equalsIgnoreCase("available"))
+			  {
+			availableSpecialists.add(specialists);
+			  }
+	  }
+		  return availableSpecialists;
+	}
+
+	
+	
+	
+
+}
